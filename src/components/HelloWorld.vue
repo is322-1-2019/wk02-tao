ec0640c28d754f6e9ce3547dff072278633c914c
wolf20099 <tonsakmaicharoen@gmail.com>
2019-08-28 13:48:25 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,31 @@
 <template>
 <div class="container">
-  <h2>Hover Rows</h2>
-  <p>The .table-hover class enables a hover state (grey background on mouse over) on table rows:</p>            
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Dominica</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
 
 
 
-  <h2>Progress Bar With Label</h2>
+
+  <h2>Stay focused, be present</h2>
+              
+  <br /> 
+  <br /> 
+  <br /> 
+
+
   <div class="progress">
     <div class="progress-bar" style="width:90%">90%</div>
   </div>
 
-<h2>Animated Progress Bar</h2>
-  <p>Add the .progress-bar-animated class to animate the progress bar:</p> 
-  <div class="progress">
-    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:40%"></div>
-  </div>
+
+
+<br /> 
+<br /> 
 
 
 
-<h2>Stretched Link in Card</h2>
-  <p>Add the .stretched-link class to a link inside the card, and it will make the whole card clickable (the card will act as a link):</p>
-  <div class="card" style="width:400px">
-    <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
-    <div class="card-body">
-      <h4 class="card-title">John Doe</h4>
-      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-      <a href="#" class="btn btn-primary stretched-link">See Profile</a>
-    </div>
-  </div>
+
+<br /> 
+<br /> 
+  <button type="button" class="btn btn-success">Plant</button>
 
 
 
@@ -68,10 +40,44 @@ export default {
     msg: String
   }
 }
+
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style >
 h3 {
   margin: 40px 0 0;
 }
@@ -86,4 +92,19 @@ li {
 a {
   color: #42b983;
 }
+
+
+body  {
+  
+ background-image: url("https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/YqGSkmb/abstract-natural-green-organic-motion-background_s_knbmmm__F0007.png");
+}
+
+
+p {
+  text-align: center;
+  font-size: 60px;
+  margin-top: 0px;
+}
+
+
 </style>
